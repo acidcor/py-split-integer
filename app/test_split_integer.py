3e4fd83f6f147687c9224c059748059eba88c1b6
acidcor <deadfoxua1337@gmail.com>
2025-10-19 20:07:57 +0200

    added new tests fix old

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,7 +2,21 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    assert sum(split_integer(8, 2)) == 8
+    value, parts = 39, 6
+    result = split_integer(value, parts)
+    assert sum(result) == value
+
+
+def test_difference_max_min_num() -> None:
+    value, parts = 55, 3
+    result = split_integer(value, parts)
+    assert max(result) - min(result) == 1
+
+
+def test_all_integer() -> None:
+    value, parts = 32, 3
+    result = split_integer(value, parts)
+    assert all(isinstance(number, int) for number in result)
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
@@ -10,11 +24,12 @@ def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    assert split_integer(100, 1) == [100]
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    assert split_integer(321, 7) == sorted(split_integer(321, 7))
+    resalt = split_integer(321, 7)
+    assert resalt == sorted(resalt)
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
